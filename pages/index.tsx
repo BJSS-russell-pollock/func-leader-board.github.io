6e4c24ca96d1908052ed6183ccaf60fe170ec19b
Russell Pollock <russell.pollock@bjss.com>
2021-08-17 10:28:16 +0100

    Trying to get deploy working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Test Leader Board</title>
         <meta name="description" content="BJSS test leader board" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Welcome to the home page
         </h1>
         </main>
