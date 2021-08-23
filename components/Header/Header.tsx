f06e580e6007a729695328ebe2138b357137aaa2
Russell Pollock <russell.pollock@bjss.com>
2021-08-23 11:32:39 +0100

    Update readme

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <>
-      <div className="bg-white" />
-      <header className="top-4 lg:top-10 h-logo-sm lg:h-logo-lg px-5 lg:px-0 mx-auto w-full">
-        <a>
-          <span>Header</span>
-        </a>
+      <header className="mx-auto w-full max-w-screen-xl px-5 mb-4 mt-auto">
+      <div>
+        <div>
+          <h4>Header</h4>
+        </div>
+      </div>
       </header>
     </>
   );
